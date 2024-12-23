3489a439cb62b548935504e06500fecbb649e68d
somel's mac mini <rafioar96@gmail.com>
2024-12-23 15:21:15 +0600

    heading added

--- src/lib/components/pages/pricing/comparePlans/ComparePlans.svelte
+++ src/lib/components/pages/pricing/comparePlans/ComparePlans.svelte
@@ -41,80 +41,85 @@
   }
 </script>
 
-<div class="m-12 border rounded-xl">
-    <Table.Root>
-      <Table.Header>
-        <Table.Row>
-          {#each plans.subscriptionTypes as subscriptionType}
-            <Table.Head class="text-center bg-gray-100 py-5 max-w-52">
-              <h3 class="text-center text-3xl font-bold text-black">
-                {subscriptionType?.title}
-              </h3>
-              <p class="text-center text-gray-400">
-                {subscriptionType?.subtitle}
-              </p>
-              <div class="mt-2">
-                {#if subscriptionType?.buttonText.length <= 1}
-                  <!-- Display message and fallback price separately -->
-                  {#if getSelectedPrice(subscriptionType, selectedType).fallback}
-                    <h3 class="font-bold ">{getSelectedPrice(subscriptionType, selectedType).message}</h3>
-                    <p class="text-xl">{getSelectedPrice(subscriptionType, selectedType).fallback}
-                        /<span class="text-gray-400 font-normal">mo</span>
-                    </p>
+<div class="mt-8">
+    <div class="text-center">
+        <h2 class="text-5xl font-bold text-black">{plans?.heading}</h2>
+    </div>
+    <div class="m-12 border rounded-xl">
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              {#each plans.subscriptionTypes as subscriptionType}
+                <Table.Head class="text-center bg-gray-100 py-5 max-w-52">
+                  <h3 class="text-center text-3xl font-bold text-black">
+                    {subscriptionType?.title}
+                  </h3>
+                  <p class="text-center text-gray-400">
+                    {subscriptionType?.subtitle}
+                  </p>
+                  <div class="mt-2">
+                    {#if subscriptionType?.buttonText.length <= 1}
+                      <!-- Display message and fallback price separately -->
+                      {#if getSelectedPrice(subscriptionType, selectedType).fallback}
+                        <h3 class="font-bold ">{getSelectedPrice(subscriptionType, selectedType).message}</h3>
+                        <p class="text-xl">{getSelectedPrice(subscriptionType, selectedType).fallback}
+                            /<span class="text-gray-400 font-normal">mo</span>
+                        </p>
+                      {:else}
+                        <h1 class="text-xl ">{getSelectedPrice(subscriptionType, selectedType).message}
+                            /<span class="text-gray-400 font-normal">mo</span>
+                        </h1>
+                      {/if}
+                    {/if}
+                  </div>
+                  {#if subscriptionType?.buttonText.length > 1}
+                    <div class="flex justify-center">
+                      <Tabs texts={subscriptionType?.buttonText} bind:selectedType />
+                    </div>
                   {:else}
-                    <h1 class="text-xl ">{getSelectedPrice(subscriptionType, selectedType).message}
-                        /<span class="text-gray-400 font-normal">mo</span>
-                    </h1>
+                    <div class="mt-4 flex justify-center">
+                      <button
+                        class="flex justify-center rounded-xl border-2
+                         border-gray-200 bg-white px-4 py-2 text-gray-400"
+                      >
+                        {subscriptionType?.buttonText}
+                      </button>
+                    </div>
                   {/if}
-                {/if}
-              </div>
-              {#if subscriptionType?.buttonText.length > 1}
-                <div class="flex justify-center">
-                  <Tabs texts={subscriptionType?.buttonText} bind:selectedType />
-                </div>
-              {:else}
-                <div class="mt-4 flex justify-center">
-                  <button
-                    class="flex justify-center rounded-xl border-2
-                     border-gray-200 bg-white px-4 py-2 text-gray-400"
-                  >
-                    {subscriptionType?.buttonText}
-                  </button>
-                </div>
-              {/if}
-            </Table.Head>
+                </Table.Head>
+              {/each}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+             <!-- Iterate through features -->
+          {#each plans?.features as feature}
+          <Table.Row class="border-b-0">
+            <Table.Cell class=" text-black text-2xl">{feature?.featureHeading}</Table.Cell>
+    
+          </Table.Row>
+          {#each feature?.featureLists as list}
+            <Table.Row class="border-b-0">
+               
+              <Table.Cell class="text-gray-400">{list.featureName}</Table.Cell>
+              <!-- Values for each subscription type -->
+              {#each list.values as value}
+                <Table.Cell class="text-center">
+                    {#if value.type === 'text'}
+                        <p>{value.text}</p>
+                    {:else if value.type === 'status' }
+                    {#if value.isAvailable}
+                    <p class="flex justify-center"><Check class="text-purple-600" size={24} /></p>
+                    {:else}
+                    <p>--</p>
+                    {/if}
+                    {/if}
+                </Table.Cell>
+              {/each}
+            </Table.Row>
           {/each}
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-         <!-- Iterate through features -->
-      {#each plans?.features as feature}
-      <Table.Row class="border-b-0">
-        <Table.Cell class=" text-black text-2xl">{feature?.featureHeading}</Table.Cell>
-
-      </Table.Row>
-      {#each feature?.featureLists as list}
-        <Table.Row class="border-b-0">
-           
-          <Table.Cell class="text-gray-400">{list.featureName}</Table.Cell>
-          <!-- Values for each subscription type -->
-          {#each list.values as value}
-            <Table.Cell class="text-center">
-                {#if value.type === 'text'}
-                    <p>{value.text}</p>
-                {:else if value.type === 'status' }
-                {#if value.isAvailable}
-                <p class="flex justify-center"><Check class="text-purple-600" size={24} /></p>
-                {:else}
-                <p>--</p>
-                {/if}
-                {/if}
-            </Table.Cell>
-          {/each}
-        </Table.Row>
-      {/each}
-    {/each}
-      </Table.Body>
-    </Table.Root>
+        {/each}
+          </Table.Body>
+        </Table.Root>
+    </div>
 </div>
   
