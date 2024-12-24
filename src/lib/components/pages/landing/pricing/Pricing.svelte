72c450bc8f25813583a9f5c28f02e4cfc7449ef0
somel's mac mini <rafioar96@gmail.com>
2024-12-25 01:31:24 +0600

    topbar added

--- src/lib/components/pages/landing/pricing/Pricing.svelte
+++ src/lib/components/pages/landing/pricing/Pricing.svelte
@@ -5,6 +5,7 @@
     PricingPageProps,
   } from '../../../../../types/landing.types';
   import Section from './Section.svelte';
+  import TopBar from './TopBar.svelte';
 
   interface Props {
     props: PricingPageProps;
@@ -14,9 +15,11 @@
   let {props, pricing}: Props = $props();
   let {sectionName, sectionIcon, sectionTitle} = $derived(props);
   let {enterprisePlan, plans, saveUpTo} = $derived(pricing);
+  let selectedSubscriptionType = $state('monthly');
   $inspect(props)
 </script>
 
-<div>
+<div class="max-w-[75rem] mx-auto">
     <Section {sectionName} {sectionIcon} {sectionTitle} />
+    <TopBar {saveUpTo} bind:selectedSubscriptionType={selectedSubscriptionType} />
 </div>
---
+++ src/lib/components/pages/landing/pricing/TopBar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { TableProperties } from 'lucide-svelte';
+  interface Props {
+    saveUpTo: number;
+    selectedSubscriptionType: string;
+  }
+  let { saveUpTo, selectedSubscriptionType = $bindable() }: Props = $props();
+  $inspect(saveUpTo);
+</script>
+
+<div class="flex  justify-between items-center mt-10">
+  <div class="rounded-xl border px-2 py-1">
+    <button
+      onclick={() => (selectedSubscriptionType = 'monthly')}
+      class={selectedSubscriptionType === 'monthly'
+        ? 'rounded-lg bg-violet-100 px-9 py-2 transition-colors delay-150 text-base font-semibold text-black'
+        : 'px-9 py-2 text-base font-semibold text-black'}>
+      Monthly
+    </button>
+    <button
+      onclick={() => (selectedSubscriptionType = 'annual')}
+      class={selectedSubscriptionType === 'annual'
+        ? 'rounded-lg bg-violet-100 px-9 py-2 transition-colors delay-150 text-base font-semibold text-black'
+        : 'px-9 py-2 text-base font-semibold text-black'}>
+      <p>
+        Annual
+        <span class="text-sm font-semibold text-violet-600">
+          (save up to {saveUpTo}%)
+        </span>
+      </p>
+    </button>
+  </div>
+  <div class="rounded-xl border px-2 py-1 flex items-center gap-2">
+    <p class="text-lg font-semibold text-black">Compare Sheet</p>
+    <TableProperties size={20} />
+  </div>
+</div>
