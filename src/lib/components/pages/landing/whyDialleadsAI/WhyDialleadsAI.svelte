019173b7af7a7c09e7c04a568675455f3a9ab082
somel's mac mini <rafioar96@gmail.com>
2024-12-28 15:10:55 +0600

    section added

--- src/lib/components/pages/landing/whyDialleadsAI/WhyDialleadsAI.svelte
+++ src/lib/components/pages/landing/whyDialleadsAI/WhyDialleadsAI.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { WhyDialleadsAI, WhyDialleadsAIProps } from "../../../../../types/landing.types";
+  import Section from "./Section.svelte";
 
     let {props}: {props: WhyDialleadsAIProps} = $props();
     let {whyDialleadsAI}: {whyDialleadsAI: WhyDialleadsAI} = $derived(props);
@@ -7,5 +8,5 @@
 </script>
 
 <div>
-
+    <Section {sectionIcon} {sectionName} {sectionTitle} />
 </div>
