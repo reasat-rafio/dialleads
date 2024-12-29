696dd041823d55075e9f35bb98a71afd9eb8b96f
somel's mac mini <rafioar96@gmail.com>
2024-12-29 10:27:51 +0600

    geist font-family added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				geist: ['"Geist Sans"', ...fontFamily.sans]
 			},
 			keyframes: {
 				"accordion-down": {
