30e7c59de4040ef91bf5e8a8683765fec80c0d5d
somel's mac mini <rafioar96@gmail.com>
2024-12-30 16:31:38 +0600

    hero schema updated

--- src/studio/schemas/pages/landing/hero.ts
+++ src/studio/schemas/pages/landing/hero.ts
@@ -7,55 +7,63 @@ const hero = defineType({
   type: 'object',
   icon: SiHomeadvisor,
   fields: [
-    defineField({
-      name: 'title',
-      type: 'string',
-      description: 'The title of the hero section',
-    }),
-    defineField({
-      name: 'subtitle',
-      type: 'string',
-      description: 'The subtitle of the hero section',
-    }),
-    defineField({
-      name: 'video_webm',
-      type: 'file',
-      title: 'WebM',
-      options: {
-        accept: 'video/webm,video/x-matroska',
-      },
-    }),
-    defineField({
-      name: 'video_hevc',
-      type: 'file',
-      title: 'MOV - HEVC',
-      options: {
-        accept: 'video/quicktime,video/mp4',
-      },
-    }),
-    defineField({
-      name: 'thumbnail',
-      type: 'image',
-      title: 'Video Thumbnail',
-      description: 'Image to be displayed as the video thumbnail',
-      options: {
-        hotspot: true,
-      },
-      validation: (Rule) =>
-        Rule.required().error('A thumbnail image is required.'),
-    }),
-    defineField({
-      name: 'link',
-      type: 'link',
-    }),
+    {
+      name: 'hero',
+      title: 'Hero',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'title',
+          type: 'string',
+          description: 'The title of the hero section',
+        }),
+        defineField({
+          name: 'subtitle',
+          type: 'string',
+          description: 'The subtitle of the hero section',
+        }),
+        defineField({
+          name: 'video_webm',
+          type: 'file',
+          title: 'WebM',
+          options: {
+            accept: 'video/webm,video/x-matroska',
+          },
+        }),
+        defineField({
+          name: 'video_hevc',
+          type: 'file',
+          title: 'MOV - HEVC',
+          options: {
+            accept: 'video/quicktime,video/mp4',
+          },
+        }),
+        defineField({
+          name: 'thumbnail',
+          type: 'image',
+          title: 'Video Thumbnail',
+          description: 'Image to be displayed as the video thumbnail',
+          options: {
+            hotspot: true,
+          },
+          validation: (Rule) =>
+            Rule.required().error('A thumbnail image is required.'),
+        }),
+        defineField({
+          name: 'link',
+          type: 'link',
+        }),
+      ],
+    },
   ],
   preview: {
     select: {
-      subtitle: 'subtitle',
+      title: 'hero.title',
+      subtitle: 'hero.subtitle',
     },
-    prepare({ subtitle }) {
+    prepare({ title, subtitle }) {
       return {
-        title: 'Hero',
+        title: title || '',
         subtitle: subtitle || '',
         media: SiHomeadvisor,
       };
