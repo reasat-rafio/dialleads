bf67feea924fa6bfe79e8c4d6c523e2bf353f432
somel's mac mini <rafioar96@gmail.com>
2025-01-02 15:30:41 +0600

    footer done

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Footer from '$lib/components/layout/Footer.svelte';
+
+  
   import Navbar from '$lib/components/layout/Navbar.svelte';
   import { urlFor } from '$lib/sanity/sanity-client';
 
