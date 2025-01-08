12588bdd99540ca2c2c9315cac9a18ec84584a8b
somel's mac mini <rafioar96@gmail.com>
2025-01-08 12:41:51 +0600

    preview added to schema

---
+++ src/lib/components/pages/landing/dataDriven/DataDriven.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { DataDrivenProps } from "../../../../../types/landing.types";
+
+    let{props}:{props: DataDrivenProps} = $props();
+    $inspect(props)
+</script>
+
+<div>
+
+</div>
