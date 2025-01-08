c90f6389265bada926d46872ba0418acd3ac1044
somel's mac mini <rafioar96@gmail.com>
2025-01-08 18:30:14 +0600

    try to fix enterprise plan

--- src/lib/components/pages/landing/pricing/Pricing.svelte
+++ src/lib/components/pages/landing/pricing/Pricing.svelte
@@ -19,7 +19,7 @@
   let selectedSubscriptionType = $state('monthly');
 </script>
 
-<div class=" mx-auto max-w-[75rem] px-5 xl:px-0">
+<div class="relative mx-auto max-w-[75rem] px-5 xl:px-0">
   <Section {props} />
   <TopBar {saveUpTo} bind:selectedSubscriptionType />
   <div class="my-20 mt-7 grid grid-cols-1 gap-7 md:grid-cols-2 xl:grid-cols-3">
@@ -27,14 +27,27 @@
       <PricingCard {plan} bind:selectedSubscriptionType {saveUpTo} />
     {/each}
   </div>
-  <div
-    class="bg-enterpricePlan-gradient h-[29.3125rem] lg:h-[19.75rem] rounded-[1.25rem] border mb-20">
-    <div class="pt-[2.56rem] lg:pl-[3.13rem]">
-      <div class="flex justify-center lg:justify-start">
+  <div class="relative mb-20">
+    <div
+      
+      class="
+     bg-enterpricePlan-gradient-mobile
+     md:bg-enterpricePlan-gradient-desktop
+    mx-auto h-[29.3125rem] w-[20.9375rem] overflow-hidden  rounded-[1.25rem] border border-[#E4E4E7] md:w-full lg:h-fit xl:w-[75rem]">
+      <SanityImage
+        class="h-full w-full bg-cover bg-center bg-no-repeat object-cover "
+        src={enterprisePlan?.banner}
+        sizes="5vw"
+        imageUrlBuilder={imgBuilder}
+        alt="SliderMainImage" />
+    </div>
+    <div
+      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 md:left-[22rem]">
+      <div class="flex justify-center lg:justify-normal">
         <div
           class="flex w-36 items-center gap-2
         rounded-full bg-[#FFFFFF33] px-3
-         py-2 text-white">
+        py-2 text-white">
           <SanityImage
             class="h-5 w-6"
             src={enterprisePlan?.sectionOptionIcon}
@@ -47,11 +60,11 @@
         </div>
       </div>
       <h3
-        class="mt-[1.56rem] text-center font-geist text-5xl font-semibold text-white lg:text-left">
+        class="mt-[0.69rem] text-center font-geist text-[1.625rem] font-semibold text-white lg:text-left lg:text-5xl">
         {enterprisePlan?.title}
       </h3>
       <p
-        class="mt-[0.56rem] text-center font-geist text-xl font-medium text-[#DCDAE0] lg:text-left">
+        class="mt-[0.56rem] text-center font-geist text-[1.25rem] font-medium text-[#DCDAE0] lg:text-left lg:text-xl">
         {enterprisePlan?.description}
       </p>
       <div class="mt-[2.25rem] flex justify-center lg:justify-start">
