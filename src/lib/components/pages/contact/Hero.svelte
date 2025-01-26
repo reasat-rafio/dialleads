5c848b4cbfe90f3d95ca306e32e982a4fa58a5b1
somel's mac mini <rafioar96@gmail.com>
2025-01-27 01:07:27 +0600

    updated

--- src/lib/components/pages/contact/Hero.svelte
+++ src/lib/components/pages/contact/Hero.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import SectionIconAndName from '$lib/components/common/sectionIconAndName.svelte';
   import Button from '$lib/components/ui/button/button.svelte';
   import { imgBuilder } from '$lib/sanity/sanity-client';
   import SanityImage from '$lib/sanity/sanity-image/sanity-image.svelte';
@@ -11,26 +12,19 @@
 
 <svelte:window bind:innerWidth={windowWidth} />
 <div
-  class="bg-hero-gradient  pt-[9.19rem] lg:mx-[0.63rem] lg:mt-[0.63rem] lg:rounded-[1.875rem] relative">
+  class="relative bg-hero-gradient pt-[9.19rem] lg:mx-[0.63rem] lg:mt-[0.63rem] lg:rounded-[1.875rem]">
   <img
     src="/grid.png"
     alt="grid overlay"
     class="pointer-events-none absolute inset-0 h-full w-full object-cover opacity-100 mix-blend-overlay" />
   <div class="relative pb-[20rem]">
     <div
-      class="container  mx-auto w-full max-w-sm text-center text-white sm:max-w-xl md:max-w-3xl lg:h-[10.5rem] lg:max-w-5xl xl:max-w-7xl">
+      class="container mx-auto w-full max-w-sm text-center text-white sm:max-w-xl md:max-w-3xl lg:h-[10.5rem] lg:max-w-5xl xl:max-w-7xl">
       <div class="z-50 flex flex-col items-center justify-center">
-        <div
-          class="mb-[0.75rem] flex gap-x-[0.5rem] rounded-full border px-[1rem] py-[0.50rem] lg:mb-[1.5rem]">
-          <SanityImage
-            class=" h-[1.25rem] w-[1.25rem]  pt-[0.2rem] lg:pt-[0.3rem]"
-            src={props?.sectionIcon}
-            sizes="05vw"
-            imageUrlBuilder={imgBuilder} />
-          <h3 class="text-lg font-medium text-white">
-            {props.sectionTitle}
-          </h3>
-        </div>
+        <SectionIconAndName
+          wrapperClass="mt-0  md:mt-[.4rem]"
+          sectionIcon={props?.sectionIcon}
+          sectionName={props?.sectionTitle} />
 
         {#if Array.isArray(props?.title[0]?.children) && Array.isArray(props?.title[1]?.children)}
           <h1
