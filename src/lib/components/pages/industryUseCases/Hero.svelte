d7c51c4a17502f6be2efcf4c68f1e8614cb97174
Mohammad Sheakh <rafioar96@gmail.com>
2025-01-27 14:17:59 +0600

    updated

--- src/lib/components/pages/industryUseCases/Hero.svelte
+++ src/lib/components/pages/industryUseCases/Hero.svelte
@@ -82,9 +82,9 @@
       class="container mx-auto pb-[4.22rem] pt-[8.47rem] text-white lg:px-24">
       <div
         class="grid w-full grid-cols-12 gap-y-[2.52rem] lg:gap-x-[110px] xl:gap-x-[100px]">
-        <div class="col-span-12 w-full lg:col-span-6 lg:w-full xl:col-span-8">
+        <div class="col-span-12 w-full lg:col-span-4 xl:col-span-8">
           <div
-            class="flex w-full flex-col text-center lg:w-[38.4rem] lg:text-left">
+            class="flex w-full flex-col items-center text-center lg:items-start lg:text-left xl:w-[38.4rem]">
             <h1
               class="mb-[1.62rem] w-fit text-center text-[1.625rem] font-semibold lg:max-w-[40.18rem] lg:text-left lg:text-[3rem]">
               <PortableText
@@ -126,17 +126,21 @@
           </div>
         </div>
         <div
-          class="col-span-12 ml-0 mr-5 w-fit max-w-[19.75rem] lg:col-span-6 xl:col-span-4 xl:ml-[2rem]">
+          class="col-span-12 mx-auto lg:col-span-8 xl:col-span-4 xl:ml-[2rem] xl:mr-0">
+          <!-- ml-0 mr-5 w-fit max-w-[19.75rem]  lg:mr-[19rem]-->
           <div
             style="background: linear-gradient(242deg, rgba(255, 255, 255, 0.21) 0%, rgba(255, 255, 255, 0.08) 100%);"
             class="w-[19rem] rounded-[1.38rem] border-[0.342px] border-gray-400 bg-transparent p-[0.63rem]">
             <div
               class="flex w-full flex-col rounded-[0.88rem] bg-white p-[0.55rem]">
-              <SanityImage
-                class="h-full w-full object-cover"
-                src={industryUseCase?.useCaseImageForAI}
-                sizes="(min-width: 1024px) 33vw, (min-width: 640px) 50vw, 100vw"
-                imageUrlBuilder={imgBuilder} />
+              <div
+                class="max-h-[215px] min-h-full overflow-hidden rounded-[0.88rem]">
+                <SanityImage
+                  class="h-full w-full object-cover"
+                  src={industryUseCase?.useCaseImageForAI}
+                  sizes="(min-width: 1024px) 33vw, (min-width: 640px) 50vw, 100vw"
+                  imageUrlBuilder={imgBuilder} />
+              </div>
               <h3
                 class="mt-[1.38rem] text-center text-[1.375rem] font-semibold text-black">
                 {industryUseCase.useCaseTitleForAI}
