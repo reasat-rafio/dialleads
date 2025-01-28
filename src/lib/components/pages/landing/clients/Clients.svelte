04ec535be3de7142f36284f3d3c0f1c78e07f3b3
somel's mac mini <rafioar96@gmail.com>
2025-01-28 10:08:48 +0600

    margin adjust

--- src/lib/components/pages/landing/clients/Clients.svelte
+++ src/lib/components/pages/landing/clients/Clients.svelte
@@ -16,7 +16,7 @@
   let api = $state<CarouselAPI>();
 </script>
 
-<div class="mb-[2.5rem] mt-[1.88rem] lg:mb-[5.25rem] lg:mt-[4.94rem]">
+<div class="mb-[2.5rem] mt-[1.88rem]  lg:mt-[4.94rem]">
   <h3 class="mb-[2rem] text-center text-[1.125rem] font-semibold">
     <PortableText
       value={props?.title}
