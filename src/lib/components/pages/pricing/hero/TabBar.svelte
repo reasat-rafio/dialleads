29aa67b2dfab7d5c2f2423dae5ab166d4f4ef1c2
somel's mac mini <rafioar96@gmail.com>
2025-01-29 13:06:16 +0600

    pricing page ui fix

--- src/lib/components/pages/pricing/hero/TabBar.svelte
+++ src/lib/components/pages/pricing/hero/TabBar.svelte
@@ -8,19 +8,19 @@
 
 <div class="mt-[1.63rem] flex justify-center px-5  lg:px-0">
   <div
-    class="flex h-full w-full items-center gap-2 rounded-xl border border-[#60527A] bg-[#2B194C] p-[0.5rem] md:w-[24rem]">
+    class="flex h-full w-full items-center gap-2 rounded-xl border border-[#60527A] bg-[#2B194C] pt-[0.4rem] pb-[0.6rem] px-[0.5rem] md:w-[24rem]">
     <button
       onclick={() => (selectedSubscriptionType = 'monthly')}
       class={selectedSubscriptionType === 'monthly'
-        ? 'flex w-full items-center justify-center rounded-[0.5rem] bg-[#5307CA] px-5 py-[0.88rem] text-base font-medium text-white'
-        : 'flex  w-full items-center justify-center  rounded-[0.5rem] px-5 py-[0.88rem] text-base font-medium text-white'}>
+        ? 'flex w-full items-center justify-center rounded-[0.5rem] bg-[#5307CA] px-5 pt-[0.88rem] pb-[0.6rem] text-base font-medium text-white'
+        : 'flex  w-full items-center justify-center  rounded-[0.5rem] px-5 pt-[0.88rem] pb-[0.6rem] text-base font-medium text-white'}>
       Monthly
     </button>
     <button
       onclick={() => (selectedSubscriptionType = 'annual')}
       class={selectedSubscriptionType === 'annual'
-        ? 'flex  w-full items-center justify-center rounded-[0.5rem] bg-[#5307CA] px-5 py-[0.88rem] text-base font-medium text-white'
-        : 'flex w-full items-center justify-center  rounded-[0.5rem] px-5 py-[0.88rem] text-base font-medium text-white'}>
+        ? 'flex  w-full items-center justify-center rounded-[0.5rem] bg-[#5307CA] px-5 pt-[0.88rem] pb-[0.6rem] text-base font-medium text-white'
+        : 'flex w-full items-center justify-center  rounded-[0.5rem] px-5 pt-[0.88rem] pb-[0.6rem] text-base font-medium text-white'}>
       <p class="text-nowrap">
         Annual
         <span class="text-sm font-medium text-white">
