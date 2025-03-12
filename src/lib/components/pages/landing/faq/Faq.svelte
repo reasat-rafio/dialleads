25eb37b7602d091115350dc8715154085a1a900f
hbithto <hasanbithto207058@gmail.com>
2025-03-12 20:14:16 +0600

    refactor: enhance FAQ accordion component with custom trigger and styling

--- src/lib/components/pages/landing/faq/Faq.svelte
+++ src/lib/components/pages/landing/faq/Faq.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import SectionIconAndName from '$lib/components/common/sectionIconAndName.svelte';
 	import * as Accordion from '$lib/components/ui/accordion/index.js';
-	import { imgBuilder } from '$lib/sanity/sanity-client';
-	import SanityImage from '$lib/sanity/sanity-image/sanity-image.svelte';
 	import type { FaqProps } from '../../../../../types/landing.types';
 
 	interface Props {
@@ -14,7 +12,7 @@
 <div class="mx-auto max-w-7xl px-5 2xl:px-0">
 	<div
 		class="mb-10
-   grid grid-cols-1 justify-center gap-x-[3rem] gap-y-[1.88rem] md:grid-cols-12 md:gap-y-[0rem] mt-[60px] lg:mt-[5.94rem] xl:mx-0"
+   mt-[60px] grid grid-cols-1 justify-center gap-x-[3rem] gap-y-[1.88rem] md:grid-cols-12 md:gap-y-[0rem] lg:mt-[5.94rem] xl:mx-0"
 	>
 		<div class="col-span-1 md:col-span-5">
 			<div class="flex flex-col items-center md:items-start">
@@ -39,13 +37,18 @@
 			<Accordion.Root type="single" class="w-full">
 				{#each props.faq as faqItem}
 					<Accordion.Item
-						class="mb-[0.75rem] rounded-[1rem]  bg-[#F9FAFB] px-[2rem] py-[1rem] lg:mb-[1rem] "
+						class="mb-[0.75rem] rounded-[1rem] border border-violet-400 bg-violet-50/80 p-0 lg:mb-[1rem]"
 						value={faqItem._key}
 					>
-						<Accordion.Trigger class="text-left hover:no-underline text-[16px] leading-7 font-medium md:text-[18px] md:leading-6 md:font-semibold">
+						<Accordion.FAQTrigger
+							class="px-[2rem] py-[2rem] text-left text-[16px] font-medium leading-4 hover:no-underline md:text-[18px] md:font-semibold md:leading-5"
+						>
 							{faqItem.question}
-						</Accordion.Trigger>
-						<Accordion.Content class="text-left text-[#5B6779] font-geist text-[14px] md:text-[16px] font-normal leading-[150%] tracking-[0%]">
+						</Accordion.FAQTrigger>
+						<Accordion.Content
+							innerclass="px-8 pb-8"
+							class="text-left font-geist text-[14px] font-normal leading-[150%] tracking-[0%] text-[#5B6779] md:text-[16px]"
+						>
 							{faqItem.answer}
 						</Accordion.Content>
 					</Accordion.Item>
