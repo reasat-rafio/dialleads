56ecee5d5f027e80885edbb5228fb7a2c4c44ee1
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-07-09 14:35:46 +0600

    fix: Adjust styling in ContactForm for improved layout and iframe integration

--- src/lib/components/pages/contact/contact-form/ContactForm.svelte
+++ src/lib/components/pages/contact/contact-form/ContactForm.svelte
@@ -9,7 +9,7 @@
 <section class="relative mx-auto mt-[2.5rem] w-full max-w-7xl px-5 lg:mt-[5rem] 2xl:px-0">
 	<div class="grid grid-cols-2 md:gap-x-[1.875rem]">
 		<SanityImage
-			class="hidden overflow-hidden rounded-2xl mt-16 md:block"
+			class="mt-16 hidden overflow-hidden rounded-2xl md:block"
 			lqip
 			sizes="50vw"
 			imageUrlBuilder={imgBuilder}
@@ -20,12 +20,12 @@
 		<div class="col-span-2 space-y-1 md:col-span-1">
 			<h2 class="app-title-1">{title}</h2>
 			<p class="app-body-3">{description}</p>
-			<div class="mt-6 w-full md:w-[572px] h-[744px] mx-auto overflow-hidden rounded-2xl">
-				<iframe 
-					src="https://api.leadconnectorhq.com/widget/booking/ApgFKhpDXGU0goWaQFMp" 
-					class="w-full h-full"
-					style="border:none;overflow: hidden;" 
-					scrolling="no" 
+			<div class="mx-auto mt-6 h-[744px] w-full overflow-hidden rounded-2xl md:w-[572px]">
+				<iframe
+					src="https://api.leadconnectorhq.com/widget/booking/ApgFKhpDXGU0goWaQFMp"
+					class="h-full w-full"
+					style="border:none;overflow: hidden;"
+					scrolling="no"
 					id="ApgFKhpDXGU0goWaQFMp_1751900196727"
 					title="Book a call"
 				></iframe>
