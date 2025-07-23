ecafff65337a6e9ca6b9fd51385716d72bea93a6
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-07-23 15:42:52 +0600

    Refactored the Industries.svelte component to apply transitions without showing an extra scrollbar.

--- src/lib/components/pages/landing/industry/Industries.svelte
+++ src/lib/components/pages/landing/industry/Industries.svelte
@@ -15,7 +15,6 @@
   const handleButtonClick = (industryNumber: number) => {
     currentIndustry = industryNumber;
   };
-
 </script>
 
 <div class="mx-auto max-w-7xl px-5 2xl:px-0">
@@ -25,13 +24,10 @@
     borderClass="border border-gray-200"
     sectionIcon={props.sectionIcon}
     sectionName={props.sectionTitle}
-    sectionTitle={props.title}
-  />
+    sectionTitle={props.title} />
 
   <div class="mt-8 flex w-full flex-col justify-between lg:mt-16 lg:flex-row lg:gap-[3.125rem] xl:gap-[4.375rem]">
-
     <div class="flex w-full lg:w-fit">
-
       <Carousel.Root class="w-full lg:hidden">
         <Carousel.Content class="-ml-4 w-full">
           {#each industries as industry, idx}
@@ -40,35 +36,30 @@
                 onclick={() => handleButtonClick(idx)}
                 class={`relative flex h-[3rem] w-[13.625rem] flex-none items-center gap-4 rounded-[4.125rem] border border-primary p-2 pr-8 transition-colors duration-100 ${
                   currentIndustry === idx ? "bg-primary" : "bg-transparent"
-                }`}
-              >
-              
-             {#if currentIndustry === idx}
-                <SanityImage
-                  class="h-fit w-fit flex-none"
-                  innerClass="object-contain w-8 h-8"
-                  src={industry.buttonIcon2}
-                  sizes="30vw"
-                  imageUrlBuilder={imgBuilder}
-                  alt="icon"
-                />
-              {:else}
-                <SanityImage
-                  class="h-fit w-fit flex-none"
-                  innerClass="object-contain w-8 h-8"
-                  src={industry.buttonIcon}
-                  sizes="30vw"
-                  imageUrlBuilder={imgBuilder}
-                  alt="icon"
-                />
-              {/if}
-          
+                }`}>
+                {#if currentIndustry === idx}
+                  <SanityImage
+                    class="h-fit w-fit flex-none"
+                    innerClass="object-contain w-8 h-8"
+                    src={industry.buttonIcon2}
+                    sizes="30vw"
+                    imageUrlBuilder={imgBuilder}
+                    alt="icon" />
+                {:else}
+                  <SanityImage
+                    class="h-fit w-fit flex-none"
+                    innerClass="object-contain w-8 h-8"
+                    src={industry.buttonIcon}
+                    sizes="30vw"
+                    imageUrlBuilder={imgBuilder}
+                    alt="icon" />
+                {/if}
+
                 <p
                   class={cn(
                     "text-nowrap font-geist text-[1.25rem] font-medium leading-[100%]",
-                    currentIndustry === idx ? "text-white" : "text-primary"
-                  )}
-                >
+                    currentIndustry === idx ? "text-white" : "text-primary",
+                  )}>
                   {industry.title}
                 </p>
               </button>
@@ -77,40 +68,35 @@
         </Carousel.Content>
       </Carousel.Root>
 
-
       <div class="hidden w-fit flex-col gap-5 lg:flex">
         {#each industries as industry, idx}
           <button
             onclick={() => handleButtonClick(idx)}
             class={`relative flex h-[3rem] w-[13.625rem] items-center gap-4 rounded-[4.125rem] border border-primary p-2 transition-colors duration-100 ${
               currentIndustry === idx ? "bg-primary" : "bg-transparent"
-            }`}
-          >
-           {#if currentIndustry === idx}
-                <SanityImage
-                  class="h-fit w-fit flex-none"
-                  innerClass="object-contain w-8 h-8"
-                  src={industry.buttonIcon2}
-                  sizes="30vw"
-                  imageUrlBuilder={imgBuilder}
-                  alt="icon"
-                />
-              {:else}
-                <SanityImage
-                  class="h-fit w-fit flex-none"
-                  innerClass="object-contain w-8 h-8"
-                  src={industry.buttonIcon}
-                  sizes="30vw"
-                  imageUrlBuilder={imgBuilder}
-                  alt="icon"
-                />
-              {/if}
+            }`}>
+            {#if currentIndustry === idx}
+              <SanityImage
+                class="h-fit w-fit flex-none"
+                innerClass="object-contain w-8 h-8"
+                src={industry.buttonIcon2}
+                sizes="30vw"
+                imageUrlBuilder={imgBuilder}
+                alt="icon" />
+            {:else}
+              <SanityImage
+                class="h-fit w-fit flex-none"
+                innerClass="object-contain w-8 h-8"
+                src={industry.buttonIcon}
+                sizes="30vw"
+                imageUrlBuilder={imgBuilder}
+                alt="icon" />
+            {/if}
 
             <h3
               class={`font-geist text-[1.25rem] font-medium leading-[100%] ${
                 currentIndustry === idx ? "text-white" : "text-primary"
-              }`}
-            >
+              }`}>
               {industry.title}
             </h3>
           </button>
@@ -118,54 +104,53 @@
       </div>
     </div>
 
+    <div class="w-full overflow-x-hidden">
+      {#each industries as industry, idx (`industry${idx}`)}
+        {#if currentIndustry === idx}
+          <div class="mt-8 flex justify-center gap-[3.125rem] lg:mt-0 xl:gap-[4.375rem]">
+            <div class="flex w-full max-w-[32rem] flex-col" in:fly={{ x: -100, duration: 600 }}>
+              <p class="w-full text-[1.125rem] leading-7 text-black opacity-85 lg:text-[1.25rem] lg:leading-[150%]">
+                {industry.description}
+              </p>
 
-    {#each industries as industry, idx (`industry${idx}`)}
-      {#if currentIndustry === idx}
-        <div class="mt-8 flex justify-center gap-[3.125rem] lg:mt-0 xl:gap-[4.375rem]">
+              <div class="mt-6 flex w-full flex-col gap-y-5 lg:mt-[2.625rem]">
+                {#each industry.useCases as useCase}
+                  <div class="flex w-full gap-4">
+                    <SanityImage
+                      class="h-fit w-fit flex-none"
+                      innerClass="object-contain w-8 h-8"
+                      src={useCaseIcon}
+                      sizes="30vw"
+                      imageUrlBuilder={imgBuilder}
+                      alt="icon" />
 
-          <div class="flex w-full max-w-[32rem] flex-col" in:fly={{ x: -100, duration: 600 }}>
-            <p class="w-full text-[1.125rem] leading-7 text-black opacity-85 lg:text-[1.25rem] lg:leading-[150%]">
-              {industry.description}
-            </p>
-
-            <div class="mt-6 flex w-full flex-col gap-y-5 lg:mt-[2.625rem]">
-              {#each industry.useCases as useCase}
-                <div class="flex w-full gap-4">
-                  <SanityImage
-                    class="h-fit w-fit flex-none"
-                    innerClass="object-contain w-8 h-8"
-                    src={useCaseIcon}
-                    sizes="30vw"
-                    imageUrlBuilder={imgBuilder}
-                    alt="icon"
-                  />
-
-                  <div class="flex w-full flex-col gap-2">
-                    <p class="font-geist text-[1.125rem] font-semibold leading-6 text-black lg:text-[1.5rem] lg:tracking-[-0.044rem]">
-                      {useCase.title}
-                    </p>
-                    <p class="text-[1rem] leading-[150%] text-[#737275] lg:text-[1.125rem] lg:leading-7">
-                      {useCase.description}
-                    </p>
+                    <div class="flex w-full flex-col gap-2">
+                      <p
+                        class="font-geist text-[1.125rem] font-semibold leading-6 text-black lg:text-[1.5rem] lg:tracking-[-0.044rem]">
+                        {useCase.title}
+                      </p>
+                      <p class="text-[1rem] leading-[150%] text-[#737275] lg:text-[1.125rem] lg:leading-7">
+                        {useCase.description}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              {/each}
+                {/each}
+              </div>
+            </div>
+
+            <div class="hidden h-full max-h-[25rem] w-[23.25rem] lg:block" in:fly={{ x: 100, duration: 600 }}>
+              <SanityImage
+                class="h-full w-full overflow-hidden rounded-[1.25rem]"
+                innerClass="object-cover object-center w-full h-full"
+                src={industry.useCaseImage}
+                sizes="(min-width:64rem) 12vw, 20vw"
+                imageUrlBuilder={imgBuilder}
+                alt="usecase image"
+                lqip />
             </div>
           </div>
-
-          <div class="hidden h-full max-h-[25rem] w-[23.25rem] lg:block" in:fly={{ x: 100, duration: 600 }}>
-            <SanityImage
-              class="h-full w-full overflow-hidden rounded-[1.25rem]"
-              innerClass="object-cover object-center w-full h-full"
-              src={industry.useCaseImage}
-              sizes="(min-width:64rem) 12vw, 20vw"
-              imageUrlBuilder={imgBuilder}
-              alt="usecase image"
-              lqip
-            />
-          </div>
-        </div>
-      {/if}
-    {/each}
+        {/if}
+      {/each}
+    </div>
   </div>
 </div>
